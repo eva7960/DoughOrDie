561e3a960d7cbe96cb831a439a7678a398c051e5
Eva Howard <7960.eva@gmail.com>
2025-01-20 14:59:44 -0800

    working on player movement

--- player.js
+++ player.js
@@ -25,8 +25,16 @@ class Player {
 	        }
 	    }
 	    //idle + 4 sates
-	    //
-	    this.animations[0][0] = new Animator(this.spritesheet, 210, 0, 16, 16, 1, 0.33, 14, false, true);
+	    //sprite sheet x, sprite sheet y, widht, height, franes, frame duration, padding between frames, repeat, reverse animation, loop
+	    this.animations[0][0] = new Animator(this.spritesheet, 0, 103, 33, 37, 1, 0.33, 14, false, true); //idle left
+	    this.animations[0][1] = new Animator(this.spritesheet, 0, 40, 37, 1, 0.33, 14, false, true); //idle right
+	    this.animations[0][2] = new Animator(this.spritesheet, 0, 70, 33, 37, 1, 0.33, 14, false, true); //idle up
+	    this.animations[0][3] = new Animator(this.spritesheet, 0, 10, 33, 37, 1, 0.33, 14, false, true); //idle down
+
+	    this.animations[1][0] = new Animator(this.spritesheet, 0, 105, 33, 37, 4, 0.33, 14, false, true); //walking left
+        this.animations[1][1] = new Animator(this.spritesheet, 0, 40, 37, 4, 0.33, 14, false, true); //walking right
+        this.animations[1][2] = new Animator(this.spritesheet, 0, 70, 33, 37, 4, 0.33, 14, false, true); //walking up
+        this.animations[1][3] = new Animator(this.spritesheet, 0, 10, 33, 37, 4, 0.33, 14, false, true); //walking down
 	};
 
 
@@ -35,6 +43,22 @@ class Player {
 	};
 
 	draw(ctx) {
-		ctx.drawImage(ASSET_MANAGER.getAsset("./EmptyShop.png"), 0, 0, ctx.canvas.width, ctx.canvas.height);
+		if (this.dead) {
+                    this.deadAnim.drawFrame(this.game.clockTick, ctx, this.x - this.game.camera.x, this.y, PARAMS.SCALE);
+                } else if (this.size == 2 && this.game.B && this.throwFireballTimeElapsed < 0.1) { // throwing fireballs
+                    if (this.facing == 0) {
+                        ctx.drawImage(this.spritesheet, 287, 122, 16, 32, this.x - this.game.camera.x, this.y, PARAMS.BLOCKWIDTH, 2 * PARAMS.BLOCKWIDTH);
+                    } else {
+                        ctx.drawImage(this.spritesheet, 102, 122, 16, 32, this.x - this.game.camera.x, this.y, PARAMS.BLOCKWIDTH, 2 * PARAMS.BLOCKWIDTH);
+                    }
+                } else if (this.disappear) {
+
+                } else {
+                    this.animations[this.state][this.size][this.facing].drawFrame(this.game.clockTick, ctx, this.x - this.game.camera.x, this.y, PARAMS.SCALE);
+                }
+                if (PARAMS.DEBUG) {
+                    ctx.strokeStyle = 'Red';
+                    ctx.strokeRect(this.BB.x - this.game.camera.x, this.BB.y, this.BB.width, this.BB.height);
+                }
 	};
 }
