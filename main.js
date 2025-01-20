221c703c907723c55075e6c8e940901f4a876f7b
2frogfrog <2frogfrog@gmail.com>
2025-01-20 15:33:50 -0800

    Started customer animation

--- main.js
+++ main.js
@@ -2,13 +2,15 @@ const gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
 
-ASSET_MANAGER.queueDownload("./backgrounds/EmptyShop.png")
+ASSET_MANAGER.queueDownload("./backgrounds/EmptyShop.png");
+ASSET_MANAGER.queueDownload("./sprites/customer1.png");
 
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
-
-    gameEngine.addEntity(new Shop());
+    ctx.imageSmoothingEnabled = false;
+    //gameEngine.addEntity(new Shop());
+    gameEngine.addEntity(new customer(gameEngine));
 	gameEngine.init(ctx);
 	gameEngine.start();
 });
