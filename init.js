3497f24a1bbdde2f156f2efece92d0a64bac5df5
Eva Howard <7960.eva@gmail.com>
2025-02-01 22:40:03 -0800

    deleting and pulling from main

--- init.js
+++
@@ -1,9 +0,0 @@
-(function () {
-
-  //creates asset manager and game container
-  const overworld = new Overworld({
-    element: document.querySelector(".game-container")
-  });
-  overworld.init();
-
-})();
