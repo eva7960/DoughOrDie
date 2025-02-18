d247a207488102502ec8bd0bec93b0bc42a36aba
2frogfrog <2frogfrog@gmail.com>
2025-02-18 14:11:08 -0800

    cheese

--- Cheese.js
+++ Cheese.js
@@ -11,22 +11,37 @@ class Cheese extends GameObject {
       "right": ["x", 1],
     }
   }
-  update() {
-    const nextPosition = utils.nextPosition(this.x, this.y, this.direction);
-
-    // Check for collision with all gameObjects at the next position
-    Object.keys(window.OverworldMaps.Outside.gameObjects).forEach(key => {
-      let object = window.OverworldMaps.Outside.gameObjects[key];
-      if (object instanceof Person && utils.collide(this, object)) {
-        object.hit(); // Apply hit if collision detected
-        delete window.OverworldMaps.Outside.gameObjects[this.id];
-      } else {
-        this.x = nextPosition.x;
-        this.y = nextPosition.y;
-        this.sprite.updateAnimationProgress();
+  update(state) {
+      if (this.speed > 0) {
+        this.speed--;
+        return;
       }
-    });
+      this.speed = 2; // Adjust to control speed
+
+      const nextPosition = utils.nextPosition(this.x, this.y, this.direction);
+      const directions = ["up", "down", "left", "right"];
+
+      if (state.map.isSpaceTaken(this.x, this.y, this.direction)) {
+        let newDirection;
+        do {
+          newDirection = directions[Math.floor(Math.random() * directions.length)];
+        } while (newDirection === this.direction); // Ensure different direction
+        this.direction = newDirection;
+      }
+
+      Object.keys(window.OverworldMaps.Outside.gameObjects).forEach(key => {
+        let object = window.OverworldMaps.Outside.gameObjects[key];
+        if (object instanceof Person && utils.collide(this, object)) {
+          object.hit(); // Apply hit if collision detected
+          delete window.OverworldMaps.Outside.gameObjects[this.id];
+        } else {
+          this.x = nextPosition.x;
+          this.y = nextPosition.y;
+          this.sprite.updateAnimationProgress();
+        }
+      });
   }
+
   hit() {
     console.log(this.health)
     this.health = Math.max(this.health - 10, 0);
