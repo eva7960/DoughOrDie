6f7e9db69bdcf4c9497dcfbb9cae506b1ac4e281
Eva Howard <7960.eva@gmail.com>
2025-03-11 15:00:13 -0700

    updated timer

--- Timer.js
+++ Timer.js
@@ -1,7 +1,7 @@
 class Timer {
     constructor() {
-        this.initialTime = 60;
-        this.remainingTime = 60;
+        this.initialTime = 100;
+        this.remainingTime = 100;
         this.interval = null;
     }
 
