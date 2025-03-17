3b1c8b9eccb824db8c7f1bcc214d73af6e17368b
2frogfrog <2frogfrog@gmail.com>
2025-03-16 23:11:10 -0700

    added H for help in HUD

--- HUD.js
+++ HUD.js
@@ -7,14 +7,19 @@ class HUD {
     this.scoreElement = document.createElement("div");
     this.healthElement = document.createElement("div");
     this.timerElement = document.createElement("div");
+    this.helpElement = document.createElement("div");
     //for css
     this.scoreElement.classList.add("hud-stat");
     this.healthElement.classList.add("hud-stat");
     this.timerElement.classList.add("hud-stat");
+    this.helpElement.classList.add("hud-help");
+    this.helpElement.innerText = "Press H for help";
+
 
     this.element.appendChild(this.scoreElement);
     this.element.appendChild(this.healthElement);
     this.element.appendChild(this.timerElement);
+    this.element.appendChild(this.helpElement);
 
     container.appendChild(this.element);
   }
